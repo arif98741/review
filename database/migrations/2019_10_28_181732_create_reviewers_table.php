34eaea2e03a0e25395499701a909c9eb448afe63
Ariful Islam <arif98741@gmail.com>
2019-10-29 00:50:16 +0600

    first commit

---
+++ database/migrations/2019_10_28_181732_create_reviewers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviewers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('image')->nullable();
+            $table->string('address')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reviewers');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::get('/login', 'Admin\Auth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'Admin\Auth\LoginController@login');
+  Route::post('/logout', 'Admin\Auth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'Admin\Auth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'Admin\Auth\RegisterController@register');
+
+  Route::post('/password/email', 'Admin\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'Admin\Auth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'Admin\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'Admin\Auth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'reviewer'], function () {
+  Route::get('/login', 'Reviewer\Auth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'Reviewer\Auth\LoginController@login');
+  Route::post('/logout', 'Reviewer\Auth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'Reviewer\Auth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'Reviewer\Auth\RegisterController@register');
+
+  Route::post('/password/email', 'Reviewer\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'Reviewer\Auth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'Reviewer\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'Reviewer\Auth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'company'], function () {
+  Route::get('/login', 'Company\Auth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'Company\Auth\LoginController@login');
+  Route::post('/logout', 'Company\Auth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'Company\Auth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'Company\Auth\RegisterController@register');
+
+  Route::post('/password/email', 'Company\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'Company\Auth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'Company\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'Company\Auth\ResetPasswordController@showResetForm');
+});
